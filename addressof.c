1faddf326433ae05eded2a671f70d3abcab9a9dd
exv1p3r <edsoncervantes@protonmail.com>
2016-12-15 12:51:52 -0600

    Modifiy 2 files

--- addressof.c
+++ addressof.c
@@ -4,9 +4,9 @@ int main(){
     int int_var = 5;
     int *int_ptr;
 
-    int_ptr = &int_var; //put the address of int_var into int_ptr
+   int_ptr = &int_var; //put the address of int_var into int_ptr
 }
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
-=======
->>>>>>> c3e0834b769b0fd9464b8d629f5072ea29b2ec6a
+//=======
+//>>>>>>> c3e0834b769b0fd9464b8d629f5072ea29b2ec6a
