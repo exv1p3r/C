72dc77cfc5f5d72d270fae0b330e7f17b9ac71f4
exv1p3r <edsoncervantes@protonmail.com>
2016-12-16 11:43:26 -0600

    Modified the file addressof.c

--- addressof.c
+++ addressof.c
@@ -6,10 +6,10 @@ int main(){
 
    int_ptr = &int_var; //put the address of int_var into int_ptr
 }
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //<<<<<<< HEAD
 
 //=======
 //>>>>>>> c3e0834b769b0fd9464b8d629f5072ea29b2ec6a
-=======
->>>>>>> ee84b3dcb4081f274207853c8aa72f5fcd284583
+//=======
+//>>>>>>> ee84b3dcb4081f274207853c8aa72f5fcd284583
