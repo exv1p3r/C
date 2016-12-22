62057cc2e5c1f2a0c062b36d9e64a1ded1e1970c
exv1p3r <edsoncervantes@protonmail.com>
2016-12-22 10:04:29 -0600

    Fueron modificados algunos archivos, borrando algunos comentarios y lineas de mas

--- pointer.c
+++ pointer.c
@@ -2,16 +2,16 @@
 #include <string.h>
 
 int main(){
-    char str_a[20];
-    char *pointer;
-    char *pointer2;
+    char str_a[20]; //A 20-element character array
+    char *pointer; //A pointer, meant for a character array
+    char *pointer2; //And yet another one
 
     strcpy(str_a, "Hello world!\n");
-    pointer = str_a;
+    pointer = str_a; //Set the first pointer to the start of the array.
     printf(pointer);
 
-    pointer2 = pointer + 2;
-    printf(pointer2);
-    strcpy(pointer2, "y you guys!\n");
-    printf(pointer);
+    pointer2 = pointer + 2; //Set the second one 2 bytes further in.
+    printf(pointer2); //Print it.
+    strcpy(pointer2, "y you guys!\n"); //Copy into that spot.
+    printf(pointer); //Print again
 }
