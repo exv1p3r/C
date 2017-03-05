9b0f52325419dfbadcaff67038d63ab6c605e84a
exv1p3r <edsoncervantes@protonmail.com>
2017-03-04 21:58:49 -0600

    Some new files were added

---
+++ convert2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void usage(char *program_name){
+    printf("Usage: %s <message> <# of times to repeat>\n", program_name);
+    exit(1);
+}
+
+int main(int argc, char *argv[]){
+    int i, count;
+    //if(argc < 3)
+      //usage(argc[0])
+    count = atoi(argc[2]) //Convert the 2nd argument into an integer
+    printf("Repeating %d times...\n", count);
+    for(i=0; i < count; i++)
+        printf("%3d - %s\n", i, argv[1]); //print the first argument
+}
